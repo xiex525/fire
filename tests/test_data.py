3ce36f0d7687cf0f2d0cae26ea9f5ad071f80a66
qiaobaochen <baochenqiao@gmail.com>
2025-05-13 17:45:39 +0800

    1. support more data sources[customizable] 2. windows path support

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,12 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/fire-institute/fire/blob/master/NOTICE.txt
 
-from firefin.data.gateway import fetch_data
+from firefin.data.gateway import fetch_data, DATA_MAPS
 
+print(DATA_MAPS)
 
-data = fetch_data(["open", "test_no_data"])
+data = fetch_data(["open", "test_no_data", "cn_bond_2y"])
 
 print(data["open"])
 print(data["test_no_data"])
+print(data["cn_bond_2y"])
