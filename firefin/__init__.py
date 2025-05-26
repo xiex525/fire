880ae4d49f63540c7c8b3b3cdf9d89260ecc0a44
qiaobaochen <baochenqiao@gmail.com>
2025-05-26 10:53:02 +0800

    delete aca_eva_single.py

--- firefin/__init__.py
+++ firefin/__init__.py
@@ -6,5 +6,5 @@ from .data.gateway import fetch_data
 from .evaluation.eva_utils import compute_forward_returns, compute_ic, compute_quantile_returns
 from .evaluation.industry.evaluator import Evaluator
 from .core.plot.plots import plt_ic, plt_quantile_cumulated_end_returns, plt_quantile_cumulative_returns
-from .evaluation.academia.aca_eva_single import AcaEvaluatorModel
-from .evaluation.academia.aca_eva_mult import AcaEvaluatorModelComparison
+from .evaluation.academia.AcaEvaluatorModel import AcaEvaluatorModel
+from .evaluation.academia.AcaEvaluatorModelComparison import AcaEvaluatorModelComparison
